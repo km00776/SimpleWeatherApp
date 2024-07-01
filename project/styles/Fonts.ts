2a88e0c4dbd1f925f0f60b188a7733c87c1cc631
macbook <macbook@macbooks-MacBook-Pro.local>
2024-07-01 22:24:35 +0100

    added Poppins font

--- project/styles/Fonts.ts
+++ project/styles/Fonts.ts
@@ -1,7 +1,22 @@
+import {Platform} from 'react-native';
 
-// const Fonts = {
-//     regular: 
-//     bold: 
-//     semi-bold:
+export const POPPINS_REGULAR = Platform.select({
+  ios: 'Poppins-Regular',
+  android: 'Poppins-Regular',
+});
 
-// }
+export const POPPINS_BOLD = Platform.select({
+  ios: 'Poppins-Bold',
+  android: 'Poppins-Bold',
+});
+
+export const POPPINS_SEMI_BOLD = Platform.select({
+  ios: 'Poppins-SemiBold',
+  android: 'Poppins-SemiBold',
+});
+
+export const Fonts = {
+  regular: POPPINS_REGULAR,
+  bold: POPPINS_BOLD,
+  semi_bold: POPPINS_SEMI_BOLD,
+};
